e3925bfb4715e1ac591015323040608a03895450
Brandon Thai <bthaib@gmail.com>
2013-10-27 14:05:27 -0700

    Final version w/ comments

--- js/address-book.js
+++ js/address-book.js
@@ -14,6 +14,8 @@
 
     returns undefined (array is sorted in place)
 */
+
+
 function sortObjArray(objArray, propName) {
     if (!objArray.sort)
         throw new Error('The objArray parameter does not seem to be an array (no sort method)');
@@ -34,3 +36,41 @@ function sortObjArray(objArray, propName) {
     });
 } //sortObjArray()
 
+// This function will sort the entries, render them when the page is fully
+// loaded and ready for manipulation and add event handlers for sort buttons
+$(function() {
+    render(Employees.entries);
+
+    $('.sort-ui .btn').click(function(){
+        var sortBtn = $(this);
+        var sortAttr = sortBtn.attr('data-sortby');
+        sortObjArray(Employees.entries, sortAttr);
+        render(Employees.entries);
+    })
+});
+
+// This function will merge the objects in the passed 
+// entries array with the HTML in the <div class="template"> element.
+function render(entries) {
+    var instance;
+    var addressbook = $('.address-book');
+    var template = $('.template');
+    addressbook.hide(); //hides the entire adddress book
+    addressbook.empty(); // clears out any HTML that might already be in the <div class="address-book">
+    $.each(Employees.entries, function(){
+        instance = template.clone();
+        for(prop in this) {
+            if(prop === 'pic') {
+                instance.find('.pic').attr({ 
+                    src: this[prop],
+                    alt: 'Picture of ' + this.first + ' ' + this.last
+                });    
+            } else {
+                instance.find('.' + prop).html(this[prop]);
+            }
+            instance.removeClass('template');
+            addressbook.append(instance);
+        }   
+    });
+    addressbook.fadeIn(); //make the populated address book slowly fade in
+}
